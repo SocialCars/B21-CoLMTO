6469a2e110ef4ec6ae683487f9d288f5464b2427
Malte Aschermann <malte.aschermann@tu-clausthal.de>
2018-01-18 14:24:24 +0100

    bump sumo to v0.32.0

--- build-sumo-osx.sh
+++ build-sumo-osx.sh
@@ -2,7 +2,7 @@
 
 export CPPFLAGS="$CPPFLAGS -I/opt/X11/include/"
 export LDFLAGS="-L/opt/X11/lib"
-cd sumo/sumo &&
+cd sumo &&
 make -f Makefile.cvs &&
 ./configure --with-xerces=/usr/local --with-proj-gdal=/usr/local &&
 make -j8
