4607216a79ee57058be5c731694caa4adc571cc8
Malte Aschermann <malte.aschermann@tu-clausthal.de>
2018-02-07 12:58:14 +0100

    generalise script

--- build-sumo.sh
+++ build-sumo.sh
@@ -3,15 +3,13 @@
 OS=$(uname)
 cd sumo && \
 make -f Makefile.cvs && \
-if [[ $OS = "Darwin" ]]; then
-	export CPPFLAGS="$CPPFLAGS -I/opt/X11/include/" && \
-	export LDFLAGS="-L/opt/X11/lib" && \
-	./configure --with-xerces=/usr/local --with-proj-gdal=/usr/local
-elif [[ $OS = "FreeBSD" ]]; then
-	./configure CXX=clang++ --with-xerces-libraries=/usr/local/lib --with-proj-libraries=/usr/local/lib --with-fox-config=/usr/local/bin/fox-config
-elif [[ $OS = "Linux" ]]; then
-	./configure
-fi && \
+[ $OS = "Darwin" ] &&
+    export CPPFLAGS="$CPPFLAGS -I/opt/X11/include/" && \
+    export LDFLAGS="-L/opt/X11/lib" && \
+    ./configure --with-xerces=/usr/local --with-proj-gdal=/usr/local
+[ $OS = "FreeBSD" ] && \
+    ./configure CXX=clang++ --with-xerces-libraries=/usr/local/lib --with-proj-libraries=/usr/local/lib --with-fox-config=/usr/local/bin/fox-config
+[ $OS = "Linux" ] && \
+    ./configure
 make clean && \
-make -j && \
-cd ../..
+make -j $(getconf NPROCESSORS_CONF)
