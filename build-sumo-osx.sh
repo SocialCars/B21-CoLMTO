4f97c7c473006faefd75bee5d68dd8743da2f0e5
Malte Aschermann <masc@tu-clausthal.de>
2018-02-07 12:12:36 +0100

    generalise build script for mac/fbsd/linux

--- build-sumo-osx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export CPPFLAGS="$CPPFLAGS -I/opt/X11/include/"
-export LDFLAGS="-L/opt/X11/lib"
-cd sumo &&
-make -f Makefile.cvs &&
-./configure --with-xerces=/usr/local --with-proj-gdal=/usr/local &&
-make -j8 &&
-cd ../..
---
+++ build-sumo.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+OS=$(uname)
+cd sumo && \
+make -f Makefile.cvs && \
+make clean && \
+if [[ $OS = "Darwin" ]]; then
+	export CPPFLAGS="$CPPFLAGS -I/opt/X11/include/" && \
+	export LDFLAGS="-L/opt/X11/lib" && \
+	./configure --with-xerces=/usr/local --with-proj-gdal=/usr/local
+elif [[ $OS = "FreeBSD" ]]; then
+	./configure CXX=clang++ --with-xerces-libraries=/usr/local/lib --with-proj-libraries=/usr/local/lib --with-fox-config=/usr/local/bin/fox-config
+elif [[ $OS = "Linux" ]]; then
+	./configure
+fi && \
+make -j && \
+cd ../..
