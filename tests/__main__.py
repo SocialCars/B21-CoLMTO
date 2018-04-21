e34e2f06e08d3a5c63a10c43f78252672ae9a072
Malte Aschermann <malte.aschermann@tu-clausthal.de>
2018-04-21 20:08:21 +0200

    add main to run tests

---
+++ tests/__main__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# @package tests
+# @cond LICENSE
+# #############################################################################
+# # LGPL License                                                              #
+# #                                                                           #
+# # This file is part of the Cooperative Lane Management and Traffic flow     #
+# # Optimisation project.                                                     #
+# # Copyright (c) 2018, Malte Aschermann (malte.aschermann@tu-clausthal.de)   #
+# # This program is free software: you can redistribute it and/or modify      #
+# # it under the terms of the GNU Lesser General Public License as            #
+# # published by the Free Software Foundation, either version 3 of the        #
+# # License, or (at your option) any later version.                           #
+# #                                                                           #
+# # This program is distributed in the hope that it will be useful,           #
+# # but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+# # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+# # GNU Lesser General Public License for more details.                       #
+# #                                                                           #
+# # You should have received a copy of the GNU Lesser General Public License  #
+# # along with this program. If not, see http://www.gnu.org/licenses/         #
+# #############################################################################
+# @endcond
+'''
+colmto: Test module starter
+'''
+
+import unittest
+
+if __name__=='__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(unittest.TestLoader().discover('tests', pattern='*_test.py')) # todo: fix filenames to test_*.py
