aec1f27eb7fc800146c9edc6d31fc0c0001f111e
Malte Aschermann <masc@tu-clausthal.de>
2018-04-24 15:36:32 +0200

    configuration tests

---
+++ tests/common/test_configuration.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+# @package tests.common
+# @cond LICENSE
+# #############################################################################
+# # LGPL License                                                              #
+# #                                                                           #
+# # This file is part of the Cooperative Lane Management and Traffic flow     #
+# # Optimisation project.                                                     #
+# # Copyright (c) 2018, Malte Aschermann (malte.aschermann@tu-clausthal.de)   #
+# # This program is free software: you can redistribute it and/or modify      #
+# # it under the terms of the GNU Lesser General Public License as            #
+# # published by the Free Software Foundation, either version 3 of the        #
+# # License, or (at your option) any later version.                           #
+# #                                                                           #
+# # This program is distributed in the hope that it will be useful,           #
+# # but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+# # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+# # GNU Lesser General Public License for more details.                       #
+# #                                                                           #
+# # You should have received a copy of the GNU Lesser General Public License  #
+# # along with this program. If not, see http://www.gnu.org/licenses/         #
+# #############################################################################
+# @endcond
+'''
+colmto: Test module for common.configuration.
+'''
+
+import unittest
+import tempfile
+from pathlib import Path
+from types import MappingProxyType
+
+import colmto.common.configuration
+
+
+class Namespace(object):
+    '''Namespace similar to argparse'''
+    # pylint: disable=too-few-public-methods
+    def __init__(self, **kwargs):
+        '''Initialisation.'''
+        self.__dict__.update(kwargs)
+
+
+class TestConfiguration(unittest.TestCase):
+    '''
+    Test cases for Configuration
+    '''
+
+    def test_configuration(self):
+        '''
+        Test Configuration class
+        '''
+        with tempfile.NamedTemporaryFile() as f_tmp:
+            colmto.common.configuration.Configuration(
+                Namespace(
+                    loglevel='DEBUG',
+                    quiet=False,
+                    logfile=f_tmp.name,
+                    runconfigfile=Path(f_tmp.name),
+                    scenarioconfigfile=Path(f_tmp.name),
+                    vtypesconfigfile=Path(f_tmp.name),
+                    freshconfigs=True,
+                    headless=True,
+                    gui=False,
+                    onlyoneotlsegment=True,
+                    cse_enabled=True,
+                    runs=1,
+                    scenarios=None
+                )
+            )
+            colmto.common.configuration.Configuration(
+                Namespace(
+                    loglevel='DEBUG',
+                    quiet=False,
+                    logfile=f_tmp.name,
+                    runconfigfile=Path(f_tmp.name),
+                    scenarioconfigfile=Path(f_tmp.name),
+                    vtypesconfigfile=Path(f_tmp.name),
+                    freshconfigs=True,
+                    headless=False,
+                    gui=True,
+                    onlyoneotlsegment=False,
+                    cse_enabled=False,
+                    runs=None,
+                    scenarios=['all']
+                )
+            )
+            colmto.common.configuration.Configuration(
+                Namespace(
+                    loglevel='DEBUG',
+                    quiet=False,
+                    logfile=f_tmp.name,
+                    runconfigfile=Path(f_tmp.name),
+                    scenarioconfigfile=Path(f_tmp.name),
+                    vtypesconfigfile=Path(f_tmp.name),
+                    freshconfigs=True,
+                    headless=False,
+                    gui=True,
+                    onlyoneotlsegment=False,
+                    cse_enabled=False,
+                    runs=None,
+                    scenarios='foo'
+                )
+            )
+
+    def test_configuration_base_exceptions(self):
+        '''
+        Test for BaseExceptions
+        '''
+        with tempfile.NamedTemporaryFile() as f_tmp:
+            with self.assertRaises(BaseException):
+                colmto.common.configuration.Configuration(
+                    Namespace(
+                        loglevel='DEBUG',
+                        quiet=False,
+                        logfile=f_tmp.name,
+                        runconfigfile=None,
+                        scenarioconfigfile=Path(f_tmp.name),
+                        vtypesconfigfile=Path(f_tmp.name),
+                        freshconfigs=True,
+                        headless=True,
+                        gui=False,
+                        onlyoneotlsegment=True,
+                        cse_enabled=True,
+                        runs=1,
+                        scenarios=None
+                    )
+                )
+            with self.assertRaises(BaseException):
+                colmto.common.configuration.Configuration(
+                    Namespace(
+                        loglevel='DEBUG',
+                        quiet=False,
+                        logfile=f_tmp.name,
+                        runconfigfile=Path(f_tmp.name),
+                        scenarioconfigfile=None,
+                        vtypesconfigfile=Path(f_tmp.name),
+                        freshconfigs=True,
+                        headless=True,
+                        gui=False,
+                        onlyoneotlsegment=True,
+                        cse_enabled=True,
+                        runs=1,
+                        scenarios=None
+                    )
+                )
+
+    def test_configuration_properties(self):
+        '''
+        Test Configuration properties
+        '''
+
+        with tempfile.NamedTemporaryFile() as f_tmp:
+            l_config = colmto.common.configuration.Configuration(
+                Namespace(
+                    loglevel='DEBUG',
+                    quiet=False,
+                    logfile=f_tmp.name,
+                    runconfigfile=Path(f_tmp.name),
+                    scenarioconfigfile=Path(f_tmp.name),
+                    vtypesconfigfile=Path(f_tmp.name),
+                    freshconfigs=True,
+                    headless=True,
+                    gui=False,
+                    onlyoneotlsegment=False,
+                    cse_enabled=False,
+                    runs=1000,
+                    scenarios=['NI-B210'],
+                    scenario_dir=Path(f_tmp.name),
+                    output_dir=Path(f_tmp.name),
+                    run_prefix='foo'
+                )
+            )
+            self.assertIsInstance(l_config.run_config, MappingProxyType)
+            l_runconfig = dict(l_config.run_config)
+            del l_runconfig['colmto_version']
+            self.assertDictEqual(l_runconfig, colmto.common.configuration._DEFAULT_CONFIG_RUN)
+            self.assertIsInstance(l_config.scenario_config, MappingProxyType)
+            self.assertDictEqual(dict(l_config.scenario_config), colmto.common.configuration._DEFAULT_CONFIG_SCENARIO)
+            self.assertEqual(l_config.scenario_dir, Path(f_tmp.name))
+            self.assertIsInstance(l_config.vtypes_config, MappingProxyType)
+            self.assertDictEqual(dict(l_config.vtypes_config), colmto.common.configuration._DEFAULT_CONFIG_VTYPES)
+            self.assertEqual(l_config.output_dir, Path(f_tmp.name))
+            self.assertEqual(l_config.run_prefix, 'foo')
+
